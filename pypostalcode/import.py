d34822a2e4d38cfb5442dbf136f8be8c3c102315
Boris Verkhovskiy <boris.verk@gmail.com>
2021-03-18 11:05:12 -0400

    add script for getting timezones from coords
    move that logic out of import.py

--- pypostalcode/import.py
+++ pypostalcode/import.py
@@ -11,14 +11,11 @@ try:
 except:
     from pyzipcode.settings import db_location
 
-BEFORE_DST_DATE = datetime.datetime(2021, 2, 1)
-DST_DATE = datetime.datetime(2021, 4, 1)
-
 conn = sqlite3.connect(db_location)
 c = conn.cursor()
 
 c.execute("DROP TABLE IF EXISTS PostalCodes;")
-c.execute("CREATE TABLE PostalCodes(fsa VARCHAR(3), city TEXT, province TEXT, longitude DOUBLE, latitude DOUBLE, timezone INT, dst INT);")
+c.execute("CREATE TABLE PostalCodes(fsa VARCHAR(3), city TEXT, province TEXT, longitude DOUBLE, latitude DOUBLE, timezone DOUBLE, dst DOUBLE);")
 c.execute("CREATE INDEX fsa_index ON PostalCodes(fsa);")
 c.execute("CREATE INDEX city_index ON PostalCodes(city);")
 c.execute("CREATE INDEX province_index ON PostalCodes(province);")
@@ -27,38 +24,14 @@ reader = csv.reader(open('ca_postalcodes.csv', newline='', encoding='utf-8'))
 next(reader)  # skip the header row
 
 
-tf = TimezoneFinder()
-def coords_to_utcoffset_and_isdst(lng, lat):
-    timezone_name = tf.timezone_at(lng=lng, lat=lat)
-
-    timezone = pytz.timezone(timezone_name)
-    timezone_now = datetime.datetime.now(timezone)
-    offset = timezone.utcoffset(BEFORE_DST_DATE).total_seconds()/60/60
-
-    dst = timezone.dst(DST_DATE).total_seconds()/60/60
-
-    return offset, dst, timezone_name
-
-
-
 for row in reader:
     fsa, city, province, lat, lng, timezone, dst = row
 
     # lat,lng in the csv becomes lng,lat in the package
     lng, lat = float(lng), float(lat) 
 
-    timezone_calculated, dst_calculated, tz_name = coords_to_utcoffset_and_isdst(lng, lat)
+    timezone, dst = float(timezone), float(dst)
 
-    if not timezone or not dst:
-        timezone = timezone_calculated
-        dst = dst_calculated
-        print(fsa, city, province, lat, lng, timezone, dst)
-        print(f"setting to ({timezone_calculated},  {dst_calculated}) {tz_name}")
-
-    if (float(timezone) != timezone_calculated or float(dst) != dst_calculated) and fsa != 'H0H':
-        print(fsa, city, province, lat, lng, timezone, dst)
-        print(f"({timezone}, {dst}) should be ({timezone_calculated},  {dst_calculated}) {tz_name}")
-    
     c.execute(
         'INSERT INTO PostalCodes values(?,?,?,?,?,?,?)',
         (fsa, city, province, lng, lat, timezone, dst),
